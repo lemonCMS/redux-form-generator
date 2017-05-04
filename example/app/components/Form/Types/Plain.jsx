2970d4e3a05bc02b5bd9d3b2809bac0ee824dbcd
Raymond van Vuuren <rsvvuuren@hotmail.com>
2017-05-04 14:03:39 +0200

    added show, hide

--- example/app/components/Form/Types/Plain.jsx
+++ example/app/components/Form/Types/Plain.jsx
@@ -1,13 +1,26 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
+import _isFunction from 'lodash/isFunction';
 
 export default class Plain extends Component {
 
   static propTypes = {
-    'field': PropTypes.object.isRequired
+    'field': PropTypes.object.isRequired,
+    'checkHidden': PropTypes.func,
+    'checkShow': PropTypes.func,
   };
 
   render() {
+    if (this.props.field && this.props.field.hidden && _isFunction(this.props.field.hidden)) {
+      if (this.props.checkHidden(this.props.field.hidden()) === true) {
+        return null;
+      }
+    } else if (this.props.field && this.props.field.show && _isFunction(this.props.field.show)) {
+      if (this.props.checkShow(this.props.field.show()) !== true) {
+        return null;
+      }
+    }
+
     const createMarkup = (data) => {
       return {__html: data};
     };
